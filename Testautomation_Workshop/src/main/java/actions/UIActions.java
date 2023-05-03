ee9926b108bf96cba3d083331ea53645cb6db6ff
Salma <salmatarek3535@gmail.com>
2023-05-03 03:42:13 +0300

    update wait func in UIActions class

--- Testautomation_Workshop/src/main/java/actions/UIActions.java
+++ Testautomation_Workshop/src/main/java/actions/UIActions.java
@@ -5,10 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.List;
-//import org.testng.Assert;
 
 public class UIActions {
 
